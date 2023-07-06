41b84c8b6e8317d0f7339c5b4c78e7984c88ecaa
itsvictorfy <tohmanchik@gmail.com>
2023-07-06 12:31:28 +0300

    fix webSerInstall.sh

--- webSerInstall.sh
+++ webSerInstall.sh
@@ -1,8 +1,6 @@
+sudo apt-get update
 sudo apt-get install postgresql-client
 sudo apt install net-tools
-git clone https://github.com/itsvictorfy/flaskapp.git
-cd flaskapp
-sudo apt-get update
 sudo apt install python3-pip
 sudo apt install python3-virtualenv
 virtualenv flask
